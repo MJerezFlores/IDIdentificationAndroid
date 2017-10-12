0dcb3d0bf1e739f118a117f9003177b7ea8ab3eb
mjerezflores <macarenajerez89@gmail.com>
2017-10-12 22:39:30 +0100

    Components connected between theirs

--- app/src/main/java/com/macarena/idinformation/MainActivity.java
+++ app/src/main/java/com/macarena/idinformation/MainActivity.java
@@ -2,12 +2,42 @@ package com.macarena.idinformation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etID;
+    TextView tvMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        etID = (EditText) findViewById(R.id.etID);
+        tvMessage = (TextView) findViewById(R.id.tvMessage);
+    }
+
+    public void btnSubmit_OnClick(View v){
+
+        String data = etID.getText().toString();
+
+        String DOB = data.substring(0, 6);
+
+        int gender = Integer.parseInt(data.charAt(6)+"");
+        String sGender;
+        sGender = (gender >=5) ? "Male" : "Female";
+
+        int nationality = Integer.parseInt(data.charAt(10)+"");
+        String sNationality;
+        sNationality= (nationality==0) ? "South African" : "Permanent Resident";
+
+        tvMessage.setText("Your information from your ID number: \n" +
+                            "Date of Birth: " +DOB+ "\n" +
+                            "Gender: " +sGender+ "\n" +
+                            "Nationality: " +sNationality);
+
     }
 }
